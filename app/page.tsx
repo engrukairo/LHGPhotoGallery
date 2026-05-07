c1e7b7dad1ba93604eee34ff91cdefb3f8d75cff
Ukairo <78553806+engrukairo@users.noreply.github.com>
2026-05-07 07:54:44 +0100

    feat: implement homepage photographer listing

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import Header from "@/components/Header";
+import UserGrid from "@/components/UserGrid";
+import { users } from '@/data/users'
 
 export default function HomePage() {
   return (
     <main>
       <Header />
+      <section className='max-w-7xl mx-auto px-4 py-10'>
+              <UserGrid users={users} />
+</section>
     </main>
   );
 }
